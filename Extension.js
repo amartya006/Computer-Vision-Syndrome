1be3293c201db6e19843c8f38a689f1093ae5eca
amartya006 <amartyanigam6@gmail.com>
2021-08-15 00:04:55 +0530

    JSON Fetching Bug

--- Extension.js
+++ Extension.js
@@ -1,45 +1,7 @@
-alert("Take Some Break");
+// alert("Take Some Break");
 
-// let headers = document.getElementsByTagName("h1");
-// for (let i = 0; i < headers.length; i++) {
-//     headers[i].innerText = "Amartya Nigam";
-// }
-
-// let body = document.getElementsByTagName("body");
-// let head = document.querySelector('head');
-// let css = document.createElement('link');
-// css.setAttribute('rel', 'stylesheet');
-// css.setAttribute('href', 'popup.css');
-// head.append(css);
-
-// $ = function (id) {
-//     return document.getElementById(id);
-// }
-
-// var show = function (id) {
-//     $(id).style.display = 'block';
-// }
-// var hide = function (id) {
-//     $(id).style.display = 'none';
-// }
-// let bodyContent = body[0].innerHTML;
-// body[0].innerHTML = `<div class="popup" id="popup1">
-// <p>This is a popup!</p>
-// <p>Overlay uses <b>box-shadow</b>.</p>
-//   <p>(Doesn't block elements on background)</p>
-// <a href="#" onclick="hide('popup1')">Ok!</a>
-// </div>`;
-
-// body[0].innerHTML += bodyContent;
-
-// body[0].innerHTML += `<script
-// src="https://code.jquery.com/jquery-3.6.0.min.js"
-// integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4="
-// crossorigin="anonymous"></script>`;
-
-
-
-// show('popup1');
-
-
-
+fetch("python/mydata.json").then((response)=>{
+    return response.json();
+}).then((data)=>{
+    console.log(data);
+})
