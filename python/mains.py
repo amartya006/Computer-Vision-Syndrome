259e839d318fede7ec77fb509b585899d6ad6e6a
CodeSavitar <adhithsagit0510@gmail.com>
2021-08-14 18:15:13 +0530

    small change

--- python/mains.py
+++ python/mains.py
@@ -5,14 +5,6 @@ from functions import lighting_detector, screen_brightness
 
 cap = cv2.VideoCapture(0)
 
-'''def write_json(new_data, filename='mydata.json'):
-
-    with open(filename,'r+') as file:
-        file_data = json.load(file)
-        file_data["Details"].append(new_data)
-        file.seek(0)
-        json.dump(file_data, file, indent = 2)'''
-
 while True:
     _,img = cap.read()
     x = lighting_detector(img)
